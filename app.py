bbbef0a4a63eafdbbfd9f447d48ec9d228bc9a0f
Prabhu <prabu502@gmail.com>
2025-05-21 13:18:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 def add(a, b):
-    \"\"\"Return the sum of two numbers.\"\"\"
+ 
     return a + b
 
 def multiply(a, b):
-    \"\"\"Return the product of two numbers.\"\"\"
+   
     return a * b
 
 if __name__ == "__main__":
